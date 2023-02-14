e40613b4215946bb46facae6ede1d8e8f904d1c7
augustnisell <79811628+augustnisell@users.noreply.github.com>
2023-02-14 23:44:56 +0000

    WIP: Worked on setting up the new places form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new
+        @post["title"] = params["posts"]["title"]
+        @post["description"] = params["posts"]["description"]
+        @post["posted_on"] = params["posts"]["posted_on"]
+        @post["place_id"] = params["posts"]["place_id"]
+        @post.save
+
+        redirect_to("/posts")
+    end
 end
