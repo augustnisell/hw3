6966cf46dadabbb110825d19dd859f5a99397a6f
augustnisell <79811628+augustnisell@users.noreply.github.com>
2023-02-15 01:27:15 +0000

    Draft app completed, no data currently

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,12 @@ class PostsController < ApplicationController
 
     def create
         @post = Post.new
-        @post["title"] = params["posts"]["title"]
-        @post["description"] = params["posts"]["description"]
-        @post["posted_on"] = params["posts"]["posted_on"]
-        @post["place_id"] = params["posts"]["place_id"]
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
         @post.save
 
-        redirect_to("/posts")
+        redirect_to("/places/#{@post["place_id"]}")
     end
 end
